48140d2e3da0fd780f59d626c5b4fce1a73980ca
ashe-sparcs <wotjd1102@gmail.com>
2016-11-17 05:25:10 +0900

    Problem 3 input form complete. WARNING : css tags are in style.css. you can be ought to move the tags into php files.

--- problems/Q3.php
+++ problems/Q3.php
@@ -5,13 +5,23 @@
 <p>Insert new Laptop information into tables Product and Laptop if there is no Laptop with that model number.</p>
 <p><b>No blanks are allowed.</b></p>
 <form action="result3.php" method="get">
-	<!--Implement an input form -->
-	Manufacturer : <input type="text" name="maker"><br>
-	Model : <input type="text" name="model"><br>
-	Speed : <input type="text" name="speed"><br>
-	Hard disk size : <input type="text" name="hd"><br>
-	Screen size : <input type="text" name="screen"><br>
-	Price : <input type="text" name="desiredPrice">
+	<div>
+		  <label>Manufacturer : </label><input type="text" name="maker"><br><br>
+		  <label>Model : </label><input type="text" name="model"><br><br>
+		  <label>Speed : </label><input type="text" name="speed"><br><br>
+		  <label>RAM : </label>
+		  <select name="ram">
+		      <option value="1">1GB</option>
+			  <option value="2">2GB</option>
+			  <option value="3">4GB</option>
+			  <option value="4">8GB</option>
+		  </select>
+		  <br><br>
+		  <label>Hard disk size : </label><input type="text" name="hd"><br><br>
+		  <label>Screen size : </label><input type="text" name="screen"><br><br>
+		  <label>Price : </label><input type="text" name="price"><br><br>
+		  <input type="submit" value="Insert a Laptop" style="margin-left: 260px">
+	</div>
 </form>
 <?php 
 	include('../includes/footer.html');
