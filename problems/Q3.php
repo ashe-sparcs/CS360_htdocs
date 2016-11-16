49a5887a8096723fa687006b518e8331a8a33e33
ashe-sparcs <wotjd1102@gmail.com>
2016-11-17 03:52:33 +0900

    Problem 2 complete. working on Problem 3 input form.

--- problems/Q3.php
+++ problems/Q3.php
@@ -4,9 +4,14 @@
 ?>
 <p>Insert new Laptop information into tables Product and Laptop if there is no Laptop with that model number.</p>
 <p><b>No blanks are allowed.</b></p>
-<form action="result3" method="get">
+<form action="result3.php" method="get">
 	<!--Implement an input form -->
-
+	Manufacturer : <input type="text" name="maker"><br>
+	Model : <input type="text" name="model"><br>
+	Speed : <input type="text" name="speed"><br>
+	Hard disk size : <input type="text" name="hd"><br>
+	Screen size : <input type="text" name="screen"><br>
+	Price : <input type="text" name="desiredPrice">
 </form>
 <?php 
 	include('../includes/footer.html');
--- problems/result2.php
+++ problems/result2.php
@@ -23,11 +23,42 @@
 					)
 				);
 	*/
+	function price_diff($price1, $price2) {
+		return abs($price1 - $price2);
+	}
+	
+	function find_max($arr, $price) {
+		$max_pair = 
+				array(
+				"diff" => 0, 
+				"index" => -1
+				);
+		for ($i = 0; $i < 3; $i++) {
+			$new_diff = price_diff($arr[$i]["price"], $price);
+			if ($new_diff > $max_pair["diff"]) {
+				$max_pair["diff"] = $new_diff;
+				$max_pair["index"] = $i;
+			} 
+		}
+		return $max_pair;
+	}
 	function find_3PCs($conn,$price){
 		$queryResult = array();
 		
 		//implement..
-
+		$result = $conn->query("select * from product, pc where product.model = pc.model");
+		for ($i = 0; $i < 3; $i++) {
+			$tuple = $result->fetchRow();
+			array_push($queryResult, array("maker" => $tuple[0], "model" => $tuple[1], "ram" => $tuple[5], "hd" => $tuple[6], "price" => $tuple[7]));
+		}
+		$max_pair = find_max($queryResult, $price);
+		for ($i = 3; $tuple = $result->fetchRow(); $i++) {
+			$new_diff = price_diff($tuple[7], $price);
+			if ($new_diff < $max_pair["diff"]) {
+				$queryResult[$max_pair["index"]] = array("maker" => $tuple[0], "model" => $tuple[1], "ram" => $tuple[5], "hd" => $tuple[6], "price" => $tuple[7]);
+				$max_pair = find_max($queryResult, $price);
+			}
+		}
 		return $queryResult;
 	}
 ?>
@@ -39,7 +70,13 @@
 		if (!PEAR::isError($conn)){
 			
 			/* Implement an ouput screen*/
-
+			echo 'maker model ram hd price';
+			echo '<br>';
+			$queryResult = find_3PCs($conn, $_GET['desiredPrice']);
+			for ($i = 0; $i < 3; $i++) {
+				echo join(' ', $queryResult[$i]);
+				echo '<br>';
+			}
 			$conn->disconnect();
 		}
 		include('../includes/footer.html');
